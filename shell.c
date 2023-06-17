321b9df4c596cd1977ea12d43f3b19f4ed7cbe36
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-06-17 16:36:00 +0100

    main.h

--- shell.c
+++ shell.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <stdbool.h>
+#include "shell.h"
 
 int main(void)
 {
---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+
+
+#endif
